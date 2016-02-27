644821007e4b71fb5c3b9da48b86486a04cf8d66
Braden <braden@brazenbraden.com>
2016-02-27 17:37:01 +0200

    Updates tests to test policy check for creating a tweet.

--- spec/usecases/tweet_spec.rb
+++ spec/usecases/tweet_spec.rb
@@ -3,47 +3,39 @@ require_relative '../spec_helper'
 describe Usecase::CreateTweet do
 
   context 'Policy::User::CanCreateTweet' do
-    it 'is an instance of Policy::User::CanCreateTweet' do
-      expect(Policy::User::CanCreateTweet.new({})).to be_an_instance_of(Policy::User::CanCreateTweet)
+    it 'should raise a PolicyException if the user does not have permission to create a tweet' do
+      user = instance_double('User', permissions: { tweet: []})
+      policy = Policy::User::CanCreateTweet.new(user)
+      expect{policy.check}.to raise_error(PolicyException)
     end
 
-    it 'takes a user argument' do
-      user = {}
-      expect{Policy::User::CanCreateTweet.new(user)}.not_to raise_error
-    end
-
-    it 'has a check method' do
-      policy = Policy::User::CanCreateTweet.new({})
-      expect{policy.check}.not_to raise_error
+    it 'should not raise a PolicyException if the user does have permissions to create a tweet' do
+      user = instance_double('User', permissions: { tweet: [:can_create_tweet]})
+      policy = Policy::User::CanCreateTweet.new(user)
+      expect{policy.check}.not_to raise_exception
     end
   end
 
   context 'Repository::Memory::Tweet' do
     before :each do
-      user = instance_double('User', name: 'Braden')
-      policy = Policy::User::CanCreateTweet.new(user)
       @repo = MemoryRepository::TweetRepository.new
-      validator = Validator::TweetValidator.new
-      @usecase = Usecase::CreateTweet.new(policy, @repo, validator)
     end
 
     it 'should return a tweet id' do
-      tweet = @usecase.execute(tweet: 'First Tweet')
+      tweet = @repo.create(tweet: 'First Tweet')
       expect(tweet.id).to eql(1)
     end
 
     it 'should store 2 tweets and return them' do
-      @usecase.execute(tweet: 'This is my first tweet!')
-      @usecase.execute(tweet: 'This is my second tweet!')
-      tweet1 = @repo.find(1)
-      tweet2 = @repo.find(2)
+      tweet1 = @repo.create(tweet: 'This is my first tweet!')
+      tweet2 = @repo.create(tweet: 'This is my second tweet!')
       expect(tweet1.tweet).to eql('This is my first tweet!')
       expect(tweet2.tweet).to eql('This is my second tweet!')
     end
 
     it 'should return all tweets when calling all' do
-      @usecase.execute(tweet: 'This is my first tweet!')
-      @usecase.execute(tweet: 'This is my second tweet!')
+      @repo.create(tweet: 'This is my first tweet!')
+      @repo.create(tweet: 'This is my second tweet!')
       expect(@repo.all.count).to eql(2)
     end
 
@@ -70,7 +62,7 @@ describe Usecase::CreateTweet do
 
   context 'Usecase::Tweet' do
     before :each do
-      user = instance_double('User', name: 'Braden')
+      user = instance_double('User', name: 'Braden', permissions: { tweet: [:can_create_tweet]})
       policy = Policy::User::CanCreateTweet.new(user)
       repo = MemoryRepository::TweetRepository.new
       validator = Validator::TweetValidator.new
