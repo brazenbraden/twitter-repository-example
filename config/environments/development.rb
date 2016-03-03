e24be6f83a7e6417aa79fd591eadf6bc5df48ac7
Braden <braden@brazenbraden.com>
2016-03-03 14:08:03 +0200

    Adds devise to project for user authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  if defined? BetterErrors
+    BetterErrors::Middleware.allow_ip! '127.0.0.1/24'
+  end
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
