2789a75d0ef8eaeded8083b880d794ab5a663cb9
Braden <braden@brazenbraden.com>
2016-03-03 08:54:39 +0200

    Extracts the policy initializer into the base policy class.

--- app/policies/base_policy.rb
+++ app/policies/base_policy.rb
@@ -1,7 +1,14 @@
 class BasePolicy
   require_relative '../exceptions/policy_exception'
 
+  def initialize(user)
+    @user = user
+  end
+
   def fail(msg = 'Validation Exception thrown')
     raise PolicyException.new(msg)
   end
+
+  private
+  attr_reader :user
 end
--- app/policies/policy/user/can_create_tweet.rb
+++ app/policies/policy/user/can_create_tweet.rb
@@ -1,12 +1,6 @@
 module Policy
   module User
     class CanCreateTweet < BasePolicy
-      attr_accessor :user
-
-      def initialize(user)
-        @user = user
-      end
-
       def check
         unless user.permissions.blank? || user.permissions[:tweet].blank?
           fail unless user.permissions[:tweet].include?(:can_create_tweet)
@@ -14,9 +8,6 @@ module Policy
           fail 'Invalid permissions'
         end
       end
-
-      private
-      attr_reader :user
     end
   end
 end
