f4d0c04dc9b5b30a40bef7e331576b61d709eafb
Braden <braden@brazenbraden.com>
2016-03-13 08:06:36 +0200

    Cleans up the tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,59 +2,33 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = repo.all
-    @tweet = TweetEntity.new
   end
 
   def show
-    @view = Decorator::Tweet.new(repo.find(params[:id]))
+    @resource = Decorator::Tweet.new(repo.find(params[:id]))
   end
 
   def create
-    Usecase::CreateTweet.new(policy(:create_tweet), repo, validator(:tweet)).execute(permitted_params)
+    Usecase::CreateTweet.new(policy, repo, validator).execute(params)
     redirect_to tweets_path
   end
 
-  def update
-    entities = repo.mapper(params)
-    xxx
-    entities.each do |entity|
-      if entity.type_of(CommentEntity)
-        Usecase::CreateComment.new(policy(:create_comment), repo, validator(:comment)).execute(params[:id], entity)
-      elsif entity.type_of(ReplyEntity)
-        Usecase::CreateReply.new(policy(:create_reply), repo, validator(:reply)).execute(params[:id], entity)
-      end
-    end
-    # comment = CommentEntity.new(params[:comment_entity])
-    redirect_to tweet_path(params[:id])
-  end
-
   private
-
-  def permitted_params
-    params.require(:tweet_entity).permit(:tweet)
-  end
 
   def repo
     Repository.for(:tweet)
   end
 
-  def policy(key)
+  def policy
+    # TODO: Map current_user to UserEntity
     user = UserEntity.new
     user.name = 'Braden'
     user.permissions =  {tweet: [:can_create_tweet], comment: [:can_create_comment], reply: [:can_create_reply]}
-    {
-      create_tweet: Policy::User::CanCreateTweet.new(user),
-      create_comment: Policy::User::CanCreateComment.new(user),
-      create_reply: Policy::User::CanCreateReply.new(user)
-    }[key]
+    Policy::User::CanCreateTweet.new(user)
   end
 
-  def validator(key)
-    {
-      tweet: Validator::TweetValidator.new,
-      comment: Validator::CommentValidator.new,
-      reply: Validator::ReplyValidator.new
-    }[key]
+  def validator
+      Validator::TweetValidator.new
   end
 
 end
