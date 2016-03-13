c20d6fa1fdc89ae562dbfe130d6777ad8d617d6e
Braden <braden@brazenbraden.com>
2016-03-13 08:07:51 +0200

    Adds id fields to comment and reply entity to allow for backwards association traversal

--- app/entities/comment_entity.rb
+++ app/entities/comment_entity.rb
@@ -1,5 +1,6 @@
 class CommentEntity < BaseEntity
   attribute :id, Integer
+  attribute :tweet_id, Integer
   attribute :user_id, Integer
   attribute :comment, String
   attribute :created_at, Time
--- app/entities/reply_entity.rb
+++ app/entities/reply_entity.rb
@@ -1,5 +1,6 @@
 class ReplyEntity < BaseEntity
   attribute :id, Integer
+  attribute :comment_id, Integer
   attribute :content, String
   attribute :created_at, Time
 end
